697cf0699497ae803b3b41be93e7ed81d65bf599
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-11-22 15:17:44 +0000

    add IUnitOfWork interface and async method

--- src/GymManagementApp/GymManagement.Domain/IUnitOfWork.cs
+++ src/GymManagementApp/GymManagement.Domain/IUnitOfWork.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GymManagement.Domain
 {
-    internal interface IUnitOfWork
+    /// <summary>
+    /// deal with database operations -> IDisposable
+    /// bridge between repositories and rest of code
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
     {
+        // TODO: add repos here
+        Task SaveChangesAsync();
     }
 }
