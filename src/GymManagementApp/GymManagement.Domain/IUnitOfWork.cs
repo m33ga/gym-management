f8804a7055789f4d8bcc45fd425f4213cf143be8
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-11-22 11:55:11 +0000

    Add Models, Repository, SeedWork folder and uow interface

---
+++ src/GymManagementApp/GymManagement.Domain/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagement.Domain
+{
+    internal interface IUnitOfWork
+    {
+    }
+}
---
+++ src/GymManagementApp/GymManagement.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymManagement.Domain.SeedWork
+{
+    internal interface IRepository
+    {
+    }
+}
