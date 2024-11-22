fe357325d8622562c088c112eba11a4dbf551a85
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-11-22 15:16:40 +0000

    Add IRepository interface and async methods

--- src/GymManagementApp/GymManagement.Domain/SeedWork/IRepository.cs
+++ src/GymManagementApp/GymManagement.Domain/SeedWork/IRepository.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GymManagement.Domain.SeedWork
 {
-    internal interface IRepository
+    public interface IRepository<T> where T : Entity
     {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<T> FindOrCreateAsync(T entity);
+        Task<T> FindByIdAsync(int id);
+        Task<List<T>> FindAllAsync();
+
     }
 }
