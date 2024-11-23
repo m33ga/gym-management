254eb495eaeb042e12fce8b49215a172f2534d52
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-11-23 10:39:43 +0000

    Add IReviewRepository with async methods #3
    Added async methods: `AddReviewAsync`, GetReviewsByTrainerAsync`, GetReviewByMemberAndClassAsync`, and `SaveChangesAsync`.

---
+++ src/GymManagementApp/GymManagement.Domain/Repository/IReviewRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GymManagement.Domain.Models;
+using GymManagement.Domain.SeedWork;
+
+namespace GymManagement.Domain.Repository
+{
+    public interface IReviewRepository : IRepository<Review>
+    {
+        Task AddReviewAsync(Review review);
+
+        // get all reviews for a specific trainer (for the aggregate review score)
+        Task<IList<Review>> GetReviewsByTrainerAsync(int trainerId);
+
+        // get a specific review by member and class
+        Task<Review> GetReviewByMemberAndClassAsync(int memberId, int classId);
+
+        Task SaveChangesAsync();
+    }
+}
