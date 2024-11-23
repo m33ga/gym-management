ace34e35d4fb3ed2d5181721cf130e488550812b
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-11-23 10:41:30 +0000

    Add INotificationRepository interface for notifications #3
    This interface includes async methods for adding, retrieving, and marking notifications as read.

---
+++ src/GymManagementApp/GymManagement.Domain/Repository/INotificationRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GymManagement.Domain.Models;
+using GymManagement.Domain.SeedWork;
+
+namespace GymManagement.Domain.Repository
+{
+    public interface INotificationRepository : IRepository<Notification>
+    {
+        Task AddNotificationAsync(Notification notification);
+
+        // get notifications for a member
+        Task<IList<Notification>> GetNotificationsByMemberAsync(int memberId);
+
+        // get unread notifications for a member
+        Task<IList<Notification>> GetUnreadNotificationsByMemberAsync(int memberId);
+
+        Task MarkAsReadAsync(int notificationId);
+
+        Task SaveChangesAsync();
+    }
+}
