ea32346ecc481f185bf330ee242f6a991497d432
Mihai Gurduza <m320964@alunos.ipb.pt>
2024-11-23 10:30:33 +0000

    Add IClassRepository interface for managing gym classes #3
    This interface includes methods for retrieving available classes, checking schedule conflicts, adding new classes, and saving changes.

---
+++ src/GymManagementApp/GymManagement.Domain/Repository/IClassRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GymManagement.Domain.Models;
+using GymManagement.Domain.SeedWork;
+
+namespace GymManagement.Domain.Repository
+{
+    public interface IClassRepository : IRepository<Class>
+    {
+        // retrieve all available classes
+        Task<IList<Class>> GetAvailableClassesAsync();
+
+        // retrieve all classes by trainer ID
+        Task<IList<Class>> GetClassesByTrainerAsync(int trainerId);
+
+        // check for overlapping schedules when adding a class
+        Task<bool> HasScheduleConflictAsync(int trainerId, DateTime startTime, DateTime endTime);
+
+        Task AddClassAsync(Class trainingClass);
+
+        // get a class by its ID, including navigation properties
+        Task<Class> GetByIdWithDetailsAsync(int id);
+
+        // same as before (uow)
+        Task SaveChangesAsync();
+    }
+}
