ed0e110a0c7f57ed2dd2c367c6a1bc1813049fc2
Patricia Moraru <m320965@alunos.ipb.pt>
2024-11-24 00:49:58 +0000

    Fix #4: Added DbSet properties to GymManagementDbContext

--- src/GymManagementApp/GymManagement.Infrastructure/GymManagementDbContext.cs
+++ src/GymManagementApp/GymManagement.Infrastructure/GymManagementDbContext.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GymManagement.Domain.Models;
+using GymManagement.Domain.SeedWork;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymManagement.Infrastructure
 {
-    internal class GymManagementDbContext
+    public class GymManagementDbContext: DbContext
     {
+        public DbSet<Admin>Admins { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<MealIngredient> MealIngredients { get; set; }
+        public DbSet<MealPlan> MealPlans { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+
     }
 }
