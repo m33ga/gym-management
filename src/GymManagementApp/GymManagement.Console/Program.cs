35be73bfd9ccc2207f9ca8a09f5b6647a37f07a6
Vladyslav Tkachuk <m320963@alunos.ipb.pt>
2024-11-27 15:54:27 +0000

    Test Booking №3 (Add member test) #9

--- src/GymManagementApp/GymManagement.Console/Program.cs
+++ src/GymManagementApp/GymManagement.Console/Program.cs
@@ -13,6 +13,9 @@ static async Task AddBookingAsync()
       
         Membership mb1 = new("Test", 150, "Test Description", 15, 50);
         await uow.Memberships.AddMembershipAsync(mb1);
+        
+        Member m1 = new("TestName", "test@gmail.com", "password", "testuser1", "+123456789", 82.1F, 1.91F, 15, mb1);
+        await uow.Members.AddMemberAsync(m1);
 
         await uow.SaveChangesAsync();
     }
