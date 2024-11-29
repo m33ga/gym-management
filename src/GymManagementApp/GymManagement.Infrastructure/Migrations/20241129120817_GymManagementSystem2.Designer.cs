be67a591ecbeb4890a534a76c60b8f776cf6d155
Patricia Moraru <m320965@alunos.ipb.pt>
2024-11-29 12:21:11 +0000

    Add new migration #10

--- src/GymManagementApp/GymManagement.Infrastructure/Migrations/20241129120817_GymManagementSystem2.Designer.cs
+++ src/GymManagementApp/GymManagement.Infrastructure/Migrations/20241129120817_GymManagementSystem2.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GymManagement.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GymManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(GymManagementDbContext))]
-    partial class GymManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241129120817_GymManagementSystem2")]
+    partial class GymManagementSystem2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
