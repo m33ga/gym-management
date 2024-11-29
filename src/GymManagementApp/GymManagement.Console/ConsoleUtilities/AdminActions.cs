6b194e9f2ff5ca1225afab598594f69cb83a3b4e
Patricia Moraru <m320965@alunos.ipb.pt>
2024-11-29 16:25:47 +0000

    Repartization of methods in specific files

---
+++ src/GymManagementApp/GymManagement.Console/ConsoleUtilities/AdminActions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.Console.ConsoleUtilities
+{
+    public static class AdminActions
+    {
+        public static async Task HandleAdminActionsAsync(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    await MembershipActions.AddMemberShipAsync();
+                    break;
+                case "2":
+                    await MemberActions.AddMemberAsync();
+                    break;
+                case "3":
+                    await TrainerActions.AddTrainerAsync();
+                    break;
+                case "4":
+                    await MemberActions.PrintBookingsAsync();
+                    break;
+                case "5":
+                    await MemberActions.PrintMembersAsync();
+                    break;
+                case "6":
+                    await TrainerActions.PrintTrainersAsync();
+                    break;
+                case "7":
+                    await MembershipActions.PrintMemberShipsAsync();
+                    break;
+                case "8":
+                    await MemberActions.DeleteMemberAsync();
+                    break;
+                default:
+                    System.Console.WriteLine("Invalid Option.");
+                    break;
+            }
+        }
+    }
+
+}
---
+++ src/GymManagementApp/GymManagement.Console/ConsoleUtilities/ClassActions.cs
@@ -0,0 +1,55 @@
+﻿using GymManagement.Domain.Models;
+using GymManagement.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.Console.ConsoleUtilities
+{
+    public static class ClassActions
+    {
+        public static async Task AddClassAsync()
+        {
+            using (var uow = new UnitOfWork())
+            {
+                DateTime date1 = new(2025, 11, 26);
+                Trainer t1 = new("Test Name", "password", "test@gmail.com", "testuser", "+123456789");
+                Class c1 = new("TestName", "TestDescription", date1, 50, t1);
+                await uow.Classes.AddClassAsync(c1);
+                await uow.SaveChangesAsync();
+            }
+
+        }
+
+        public static async Task PrintClassesAsync()
+        {
+            using (var uow = new UnitOfWork())
+            {
+                System.Console.WriteLine($"Database path: {uow.GetDbPath()}");
+
+                var list = await uow.Classes.FindAllAsync();
+                if (list.Count == 0)
+                {
+                    System.Console.WriteLine("\n There are no Members yet");
+                }
+                else
+                {
+                    System.Console.WriteLine("\n Members:");
+                    foreach (var classi in list)
+                    {
+                        System.Console.WriteLine($"class: {classi.Id}, " +
+                            $"class Name: {classi.Name}, " +
+                            $"class Description: {classi.Description}, " +
+                            $"class Date: {classi.ScheduledDate}, " +
+                            $"class Duration: {classi.DurationInMinutes}min, " +
+                            $"class Trainer ID: {classi.TrainerId}, " +
+                            $"Class Availability: {classi.IsAvailable}, " +
+                            $"Class Member ID: {classi.MemberId} ");
+                    }
+                }
+            }
+        }
+    }
+}
