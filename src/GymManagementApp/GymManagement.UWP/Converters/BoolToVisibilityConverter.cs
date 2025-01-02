be4c3aa2c1ce8e960213f92de170dec905d009c3
Vladyslav Tkachuk <m320963@alunos.ipb.pt>
2025-01-02 13:48:58 +0000

    Created Bool to Visibility Converter

---
+++ src/GymManagementApp/GymManagement.UWP/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace GymManagement.UWP.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool isVisible)
+            {
+                if (isVisible)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
