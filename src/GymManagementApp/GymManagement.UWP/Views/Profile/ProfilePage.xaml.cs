04d6cfda54352f379f4eb6255ab8d087b3fb1b30
Patricia Moraru <m320965@alunos.ipb.pt>
2025-01-05 13:53:44 +0000

    Started setting up the ProfilePage, edited the .xaml and .xaml.cs files

--- src/GymManagementApp/GymManagement.UWP/Views/Profile/ProfilePage.xaml.cs
+++ src/GymManagementApp/GymManagement.UWP/Views/Profile/ProfilePage.xaml.cs
@@ -1,31 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using GymManagement.UWP.ViewModels;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using GymManagement.UWP.Views.Booking;
+using GymManagement.UWP.Views.Users;
 
 namespace GymManagement.UWP.Views.Profile
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ProfilePage : Page
     {
+        public ProfileViewModel ViewModel { get; }
+
         public ProfilePage()
         {
-            
             this.InitializeComponent();
+            ViewModel = new ProfileViewModel(App.UserViewModel);
+            DataContext = ViewModel;
+
+            UpdateVisibility();
+        }
+
+        private void UpdateVisibility()
+        {
+            MemberSection.Visibility = ViewModel.IsMember ?
+                Visibility.Visible : Visibility.Collapsed;
+
+            TrainerSection.Visibility = ViewModel.IsTrainer ?
+                Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void NvMain_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItemContainer is NavigationViewItem selectedItem)
+            {
+                switch (selectedItem.Tag)
+                {
+                    case "profile":
+                        frmMain.Navigate(typeof(ProfilePage));
+                        break;
+                    case "notifications":
+                        frmMain.Navigate(typeof(NotificationsPage));
+                        break;
+                    case "dashboard":
+                        frmMain.Navigate(typeof(DashboardPage));
+                        break;
+                    case "schedule":
+                        NavigateToSchedule();
+                        break;
+                    case "meal_plan":
+                        frmMain.Navigate(typeof(MealPlanPage));
+                        break;
+                }
+            }
+        }
+
+        private void NavigateToSchedule()
+        {
+            if (ViewModel.IsMember)
+            {
+                frmMain.Navigate(typeof(BookingManagementPage));
+            }
+            else if (ViewModel.IsTrainer || ViewModel.IsAdmin)
+            {
+                frmMain.Navigate(typeof(ScheduleManagementPage));
+            }
+        }
+
+        private void NvLogout_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            App.UserViewModel.DoLogout();
+            Frame.Navigate(typeof(LoginDialog));
         }
     }
 }
