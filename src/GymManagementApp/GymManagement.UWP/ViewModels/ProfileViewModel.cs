2ef6b38ead7090a26391a730479e5617b51c5c01
Patricia Moraru <m320965@alunos.ipb.pt>
2025-01-05 13:53:02 +0000

    Added basic configuration for ProfileViewModel

--- src/GymManagementApp/GymManagement.UWP/ViewModels/ProfileViewModel.cs
+++ src/GymManagementApp/GymManagement.UWP/ViewModels/ProfileViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GymManagement.UWP.ViewModels;
+using GymManagement.Domain.Models;
 
 namespace GymManagement.UWP.ViewModels
 {
     public class ProfileViewModel : BindableBase
     {
+        private readonly UserViewModel _userViewModel;
+
+        // Public parameterless constructor
+        public ProfileViewModel()
+        {
+            _userViewModel = App.UserViewModel;
+        }
+
+        public ProfileViewModel(UserViewModel userViewModel)
+        {
+            _userViewModel = userViewModel;
+        }
+
+        public string FullName => _userViewModel.FullName;
+        public string Username => _userViewModel.Username;
+        public float Weight => _userViewModel.Weight;
+        public float Height => _userViewModel.Height;
+        public Membership Membership => _userViewModel.MemberShip;
+        public bool IsMember => _userViewModel.IsMember;
+        public bool IsTrainer => _userViewModel.IsTrainer;
+        public bool IsAdmin => _userViewModel.IsAdmin;
+
+        // Profile completion validation logic
+        public bool IsProfileComplete =>
+            !string.IsNullOrWhiteSpace(FullName) &&
+            !string.IsNullOrWhiteSpace(Username) &&
+            Weight > 0 &&
+            Height > 0;
     }
 }
